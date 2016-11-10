3f40068043b7541392d33031c93a774058e236d9
Michaelbretagne <michael.bretagne@gmail.com>
2016-11-09 23:20:31 -0800

    Add the variable model

---
+++ js/script.js
@@ -0,0 +1,39 @@
+
+/*---------- Model ----------- */
+
+var model = {
+    currentDog: null,
+    dogs: [
+        {
+            clickCount : 0,
+            name : 'Medor',
+            imgSrc : 'img/1',
+            id : '1'
+        },
+        {
+            clickCount : 0,
+            name : 'Milou',
+            imgSrc : 'img/2.jpg',
+            id : '2'
+        },
+        {
+            clickCount : 0,
+            name : 'Paf',
+            imgSrc : 'img/3.jpg',
+            id : '3'
+        },
+        {
+            clickCount : 0,
+            name : 'Pouf',
+            imgSrc : 'img/4.jpg',
+            id = '4'
+        },
+        {
+            clickCount : 0,
+            name : 'Junior',
+            imgSrc : 'img/5.jpg',
+            id = '5'
+        }
+    ]
+};
+
