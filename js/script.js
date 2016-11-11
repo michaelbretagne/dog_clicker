88fe48ac8685fd4c47ce6490fc02393f2847fe30
Michaelbretagne <michael.bretagne@gmail.com>
2016-11-10 21:47:19 -0800

    feature: Add variables octopus, dogListView and dogView
    Implement a Model View Octopus pattern. The variable octopus connects the model and the view providing a separator between the view(DOM Elements) and the model(data).

--- js/script.js
+++ js/script.js
@@ -1,39 +1,115 @@
 
-/*---------- Model ----------- */
-
 var model = {
     currentDog: null,
     dogs: [
         {
             clickCount : 0,
             name : 'Medor',
-            imgSrc : 'img/1',
+            img : 'img/dog1.jpg',
             id : '1'
         },
         {
             clickCount : 0,
             name : 'Milou',
-            imgSrc : 'img/2.jpg',
+            img: 'img/dog2.jpg',
             id : '2'
         },
         {
             clickCount : 0,
             name : 'Paf',
-            imgSrc : 'img/3.jpg',
+            img : 'img/dog3.jpg',
             id : '3'
         },
         {
             clickCount : 0,
             name : 'Pouf',
-            imgSrc : 'img/4.jpg',
-            id = '4'
+            img : 'img/dog4.jpg',
+            id : '4'
         },
         {
             clickCount : 0,
             name : 'Junior',
-            imgSrc : 'img/5.jpg',
-            id = '5'
+            img : 'img/dog5.jpg',
+            id : '5'
         }
     ]
 };
 
+var octopus = {
+    init: function() {
+        model.currentDog = model.dogs[0];
+        dogListView.init();
+        dogView.init();
+    },
+
+    getCurrentDog: function () {
+        return model.currentDog;
+    },
+
+    getAllDogs: function () {
+        return model.dogs;
+    },
+
+    setCurrentDog: function(dog) {
+        model.currentDog = dog;
+    },
+
+    addToCounter: function() {
+        model.currentDog.clickCount++;
+        dogView.render();
+    }
+
+};
+
+var dogView = {
+    init: function() {
+        this.dogElem = document.getElementById('dog');
+        this.dogName = document.getElementById('dog_name');
+        this.dogImage = document.getElementById('dog_img');
+        this.dogCount = document.getElementById('dog_count');
+
+        this.dogImage.addEventListener('click', function() {
+            octopus.addToCounter();
+        });
+
+        this.render();
+    },
+
+    render: function() {
+        var currentDog = octopus.getCurrentDog();
+        this.dogCount.textContent = currentDog.clickCount;
+        this.dogName.textContent = currentDog.name;
+        this.dogImage.src = currentDog.img;
+    }
+
+};
+
+var dogListView = {
+    init: function() {
+        this.dogList = document.getElementById('dog_list');
+        this.render();
+    },
+
+    render: function() {
+        var dog, elem, i;
+        var dogs = octopus.getAllDogs();
+        this.dogList.innerHTML = '';
+
+        for (i = 0; i < dogs.length; i++) {
+            dog = dogs[i];
+
+            elem = document.createElement('li');
+            elem.textContent = dog.name;
+
+            elem.addEventListener('click', (function(dogCopy) {
+                return function() {
+                    octopus.setCurrentDog(dogCopy);
+                    dogView.render();
+                };
+            })(dog));
+
+            this.dogList.appendChild(elem);
+        }
+    }
+};
+octopus.init();
